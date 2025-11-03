c286ab27d167946dd45398afe2e6d3fa61f5eb2c
lwev <106478077+lwev@users.noreply.github.com>
2025-11-03 19:25:10 +0100

    “update20“

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: "Hundezonen - Die App für dich und deinen Hund",
   description: "Entdecke und teile die besten Hunderoutenenrouten in der Schweiz, Deutschland und Österreich. Mit Warnungen, Services und KI-Chatbot.",
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body>{children}</body>
+        <Analytics />
     </html>
   );
 }
